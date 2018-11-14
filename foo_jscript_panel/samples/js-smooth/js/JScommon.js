503f8ba20b5437da4b575565d748950093936c5a
marc2k3 <34617027+marc2k3@users.noreply.github.com>
2018-11-14 11:39:00 +0000

    Tidy up samples

--- foo_jscript_panel/samples/js-smooth/js/JScommon.js
+++ foo_jscript_panel/samples/js-smooth/js/JScommon.js
@@ -8,18 +8,6 @@ var CACHE_FOLDER = fb.ProfilePath + "js_smooth_cache\\";
 SM_CXVSCROLL = 2;
 SM_CYHSCROLL = 3;
 
-DLGC_WANTARROWS = 0x0001; /* Control wants arrow keys         */
-DLGC_WANTTAB = 0x0002; /* Control wants tab keys           */
-DLGC_WANTALLKEYS = 0x0004; /* Control wants all keys           */
-DLGC_WANTMESSAGE = 0x0004; /* Pass message to control          */
-DLGC_HASSETSEL = 0x0008; /* Understands EM_SETSEL message    */
-DLGC_DEFPUSHBUTTON = 0x0010; /* Default pushbutton               */
-DLGC_UNDEFPUSHBUTTON = 0x0020; /* Non-default pushbutton           */
-DLGC_RADIOBUTTON = 0x0040; /* Radio button                     */
-DLGC_WANTCHARS = 0x0080; /* Want WM_CHAR messages            */
-DLGC_STATIC = 0x0100; /* Static item: don't include       */
-DLGC_BUTTON = 0x2000; /* Button item: can be checked      */
-
 // Used in utils.Glob()
 // For more information, see: http://msdn.microsoft.com/en-us/library/ee332330%28VS.85%29.aspx
 FILE_ATTRIBUTE_READONLY = 0x00000001;
